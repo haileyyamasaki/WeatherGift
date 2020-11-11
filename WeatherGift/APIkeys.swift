6f61521dc18c5d4cbe8f96fc24bee202c8d773f8
Hailey Yamasaki <hyamasaki@zagmail.gonzaga.edu>
2020-11-10 19:19:08 -0800

    git ignore

---
+++ WeatherGift/APIkeys.swift
@@ -0,0 +1,14 @@
+//
+//  API Keys.swift
+//  WeatherGift
+//
+//  Created by Hailey Yamasaki on 11/10/20.
+//  Copyright © 2020 Hailey Yamasaki. All rights reserved.
+//
+
+import Foundation
+
+struct APIkeys {
+    static let googlePlacesKey = "AIzaSyBdUgQVAuhsfTfMu3jFESpnPalEZcmgArE"
+}
+
---
+++ WeatherGift/WeatherLocation.swift
@@ -0,0 +1,15 @@
+//
+//  WeatherLocation.swift
+//  WeatherGift
+//
+//  Created by Hailey Yamasaki on 11/9/20.
+//  Copyright © 2020 Hailey Yamasaki. All rights reserved.
+//
+
+import Foundation
+
+struct WeatherLocation {
+    var name: String
+    var latitude: Double
+    var longitude: Double 
+}
